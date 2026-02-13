cc31f3e2f66373cac1a077783be6966aa13ffe9b
Dagostini01 <pedro01dagostini@gmail.com>
2026-02-13 15:57:21 -0300

    refactor: port no server 3333 env

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import Fastify from "fastify";
 import { usersRoutes } from "./routes/users.js";
 import { mealRoutes } from "./routes/meal.js";
+import { env } from "./env/index.js";
 
 const app = Fastify()
 
 app.register(usersRoutes, { prefix: 'users' })
 app.register(mealRoutes, { prefix: 'meals' })
 
-app.listen({ port: 3333 })
+app.listen({ port: env.PORT })
     .then(() => {
         console.log("HTTP Server Running")
     })
